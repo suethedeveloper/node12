a7e5a921fe61a137d5fb0fc112edc19dbde7672f
Sue Lucas <choi.soyoung@gmail.com>
2019-11-29 22:27:08 -0800

    setting up the handlebar  with partials

---
+++ web-server/src/app.js
@@ -0,0 +1,74 @@
+const path = require("path")
+const express = require("express")
+const hbs = require("hbs")
+
+const app = express()
+
+//Define paths for Express config
+const publicDirectoryPath = path.join(__dirname, "../public")
+const viewsPath = path.join(__dirname, "../templates/views")
+const partialPath = path.join(__dirname, "../templates/partials")
+
+//Setup handlebars engine and views location
+app.set("view engine", "hbs")
+app.set("views", viewsPath)
+hbs.registerPartials(partialPath)
+
+//Setup static directory to serve
+app.use(express.static(publicDirectoryPath))
+
+app.get("", (req, res) => {
+    res.render("index", {
+        title: "weather app",
+        name: "abc company"
+    });
+})
+
+app.get("/about", (req, res) => {
+    res.render("about", {
+        title: "About",
+        name: "abc company"
+    });
+})
+
+app.get("/help", (req, res) => {
+    res.render("help", {
+        helpText: "Help text",
+        title: "Help",
+        name: "abc Company"
+    })
+})
+
+app.get("/help/*", (req, res) => {
+    res.render("404", {
+        title: "404",
+        name: "abc company",
+        errorMessage: "Help article not found"
+    })
+})
+
+app.get("/weather", (req, res) => {
+    res.send("show weather")
+})
+
+
+app.get("/products", (req, res) => {
+    if (!req.query.search) {
+        return res.send({error: "You must provide a search term"})
+    }
+    console.log(req.query.search);
+    res.send({
+        products: []
+    })
+})
+
+
+app.get("*", (req, res) => {
+    res.render("404", {
+        title: "404",
+        name: "abc company",
+        errorMessage: "Page not found"
+    })
+})
+
+app.listen(3000, () => console.log("Server is up on port 3000."))
