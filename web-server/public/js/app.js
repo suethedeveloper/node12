c3114317e488c7488910e18bc1a882ecc8e59ba0
Sue Lucas <choi.soyoung@gmail.com>
2019-11-30 14:50:21 -0800

    Setup app for Heroku

--- web-server/public/js/app.js
+++ web-server/public/js/app.js
@@ -11,7 +11,7 @@ watherForm.addEventListener("submit", event => {
    messageOne.textContent = "Loading...";
    messageTwo.textContent = "";
    
-   fetch("http://localhost:3000/weather?address=" + location).then(response => {
+   fetch(`/weather?address=${location}`).then(response => {
        response.json().then(data => {
            console.log(data)
            if (data.err) {
